0c271c15cee71e7875fdd2010295d2820c473a33
Ashish Verma <100463212+ashishverma5218@users.noreply.github.com>
2023-09-29 20:34:40 +0530

    ipl.py

--- ipl_win_pred/ipl.py
+++ ipl_win_pred/ipl.py
@@ -16,7 +16,7 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
        'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
        'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
-       'Sharjah', 'Mohali', 'Bengaluru']
+       'Sharjah', 'Mohali']
 
 pipe = pickle.load(open('pipe.pkl','rb'))
 st.title('IPL Win Predictor')
